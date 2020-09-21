3ca32403801f2414b12ed179c89e9ceb537667b1
hackasaur <hackasaur@vivobook14.localdomain>
2020-09-21 17:10:39 +0530

    added search bar

---
+++ precache-manifest.c3b85fdb9c8a7b3f8285c6dab662b8e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec3600ad2e84d503aa4616496987d99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c63fd0ed8b7034233f15",
+    "url": "./static/css/main.cdb8d90b.chunk.css"
+  },
+  {
+    "revision": "d4758c161dd60f90890f",
+    "url": "./static/js/2.c7f38ef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c7f38ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63fd0ed8b7034233f15",
+    "url": "./static/js/main.71f36fd3.chunk.js"
+  },
+  {
+    "revision": "ad02c076f8f67babb9ad",
+    "url": "./static/js/runtime-main.1aee6511.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
